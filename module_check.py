cf4c298042f535bb7cf4d60d39104eb80dd7dcb5
Akash Mohanty <akash.mohanty@gmail.com>
2020-02-06 05:18:24 +0000

    module check with module diag status

--- module_check.py
+++ module_check.py
@@ -4,12 +4,17 @@ with open("output_filev2.txt") as f:
 
 for line in readfile.splitlines():
     if  "N9K-X97160YC-EX" in line and "48x10/25G" in line:
-        print ("The status of card {} is {}".format(line.split()[0],line.split()[-1]))
+        pprint ("The status of card {} is {}".format(line.split()[0],line.split()[-1]))
     elif  "N9K-X9736C-FX" in line and "36x40/100G" in line:
-        print ("The status of card {} is {}".format(line.split()[0],line.split()[-1]))
+        pprint ("The status of card {} is {}".format(line.split()[0],line.split()[-1]))
     elif "N9K-C9504-FM-E" in line and "4-slot Fabric Module" in line:
-        print ("The status of card {} is {}".format(line.split()[0],line.split()[-1]))
-    elif "Online Diag Status" in line or "Pass" in line:
+        pprint ("The status of card {} is {}".format(line.split()[0],line.split()[-1]))
+    elif "logging" in line:
+        break
+print ("\n")
+for line in readfile.splitlines():
+    if "Online Diag Status" in line or "Pass" in line:
         print (line)
     elif "logging" in line:
         break
+
