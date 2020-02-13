737d095c56d02d260e9e4a9633914e451089c4a7
Akash Mohanty <akash.mohanty@gmail.com>
2020-02-13 05:57:08 +0000

    Script to sort by site, unknown number of devices

---
+++ sort_by_site.py
@@ -0,0 +1,24 @@
+from pprint import pprint
+with open("devicelist.txt") as f:
+        readfile = f.read()
+
+
+sort_by_site = [ ]
+sites = []
+
+for line in readfile.splitlines():
+    z = line.split(".")[0]
+    sites.append(z)
+
+
+sites = list(set(sites)) 
+
+
+for site in sites:
+   for line in readfile.splitlines():
+        if site in line.split(".")[0]:
+            sort_by_site.append(line)
+
+pprint (sort_by_site)
+
+        
